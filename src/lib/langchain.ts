c320a81d77489210ecf2c92b2f639b07c5c981f9
Siwon Jeon <87080940+siwonblue@users.noreply.github.com>
2023-05-27 18:42:34 +0900

    [feature/#1-express-init] VTS QA template prompt 추가

--- src/lib/langchain.ts
+++ src/lib/langchain.ts
@@ -7,9 +7,9 @@ import { openAIApiKey } from "~/constants";
 import loadVectorStore from "~/load.local.db";
 
 export const model = new ChatOpenAI({
-  temperature: 0,
+  temperature: 0.7,
   openAIApiKey,
-  verbose: false,
+  verbose: true,
   streaming: true,
   callbacks: [
     {
@@ -21,7 +21,8 @@ export const model = new ChatOpenAI({
 });
 
 const template = `This is a role-playing situation. Put yourself in the shoes of the painter Pablo Picasso. Speak once and wait for the next answer.
-I've put all of our previous conversations in a place called "context:", so if there's something you don't recognize, please refer to that and answer. 
+I've put all of our previous conversations in a place called "context:", so if there's something you don't recognize, please refer to that and answer.
+Just because the context is in JSON format doesn't mean you have to shape your answer accordingly, just make sure to reference what's in the context and answer in plain text format.
 {user}`;
 
 export const dbTemplate = `This is a role-playing situation and you're going to answer as the painter Pablo Picasso.
@@ -38,6 +39,22 @@ so if there's something you don't know, use the "context:" if you don't understa
 This conversation is about a painting called Guernica by Pablo Picasso.
 Summarize and restate what the user said and include them in the answer in the format:"Answer:" 
 Don't ask if there are any additional questions.
+Speak once and wait for the next answer.
+`;
+
+export const dbTemplateDone = `
+This is a role-playing situation and you're going to answer as the painter Pablo Picasso.
+"context:" has a history of previous conversations, Give me a full review "context:" and include them in the answer in the format:"Answer:" 
+At the end, be sure to ask if they have any additional questions.
+Speak once and wait for the next answer.
+`;
+
+export const dbTemplateQA = `
+This is a role-playing situation and you're going to answer as the painter Pablo Picasso.
+I've put all of our previous conversations in a place called "context:", so if there's something you don't recognize, please refer to that and answer.
+answer what the user said and include them in the answer in the format:"Answer:" 
+At the end, be sure to ask if they have any additional questions.
+Speak once and wait for the next answer.
 `;
 
 export async function chainInitializer({ free }: { free: boolean }) {
