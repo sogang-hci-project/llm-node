7d153ccc7cc9f57441eb78c03ee2f97ae919ad87
Siwon Jeon <87080940+siwonblue@users.noreply.github.com>
2023-05-30 16:59:15 +0900

    [develop/#3-proto] session cookie 를 사용하지 않는 방식으로 변경

--- src/controllers/post.llm.ts
+++ src/controllers/post.llm.ts
@@ -4,10 +4,10 @@ import { chainInitializer, dbTemplate, dbTemplateDone, dbTemplateNoQuiz, dbTempl
 
 export const handleChat = async (req: Request, res: Response, next: NextFunction) => {
   try {
-    const { user, sessionId, additional, done } = req.body;
+    const { user, sessionID, additional, done } = req.body;
 
-    if (!user || !sessionId) return res.status(400).json({ message: "incorrect LLM data" });
-    let context = JSON.parse(await redisClient.get(sessionId));
+    if (!user || !sessionID) return res.status(400).json({ message: "incorrect LLM data" });
+    let context = JSON.parse(await redisClient.get(sessionID));
     if (!context) context = [];
     const chat = { id: context.length + 1, human: user, ai: "" };
     context.push(chat);
@@ -25,7 +25,7 @@ export const handleChat = async (req: Request, res: Response, next: NextFunction
     });
     const { text } = result;
     context[context.length - 1]["ai"] = text;
-    redisClient.set(sessionId, JSON.stringify(context));
+    redisClient.set(sessionID, JSON.stringify(context));
 
     // 정규식을 사용하여 Answer: 뒤에 있는 문장 추출
     const answerRegex = /Answer:\s*(.*)/;
@@ -46,9 +46,9 @@ export const handleChat = async (req: Request, res: Response, next: NextFunction
 export const handleChatWithFree = async (req: Request, res: Response, next: NextFunction) => {
   try {
     const chain = await chainInitializer({ free: true });
-    const { user, sessionId } = req.body;
-    if (!user || !sessionId) return res.status(400).json({ message: "incorrect LLM data" });
-    let context = JSON.parse(await redisClient.get(sessionId));
+    const { user, sessionID } = req.body;
+    if (!user || !sessionID) return res.status(400).json({ message: "incorrect LLM data" });
+    let context = JSON.parse(await redisClient.get(sessionID));
     if (!context) context = [];
 
     let chat = { id: context.length + 1, human: user, ai: "" };
@@ -59,7 +59,7 @@ export const handleChatWithFree = async (req: Request, res: Response, next: Next
 
     context[context.length - 1]["ai"] = text;
 
-    redisClient.set(sessionId, JSON.stringify(context));
+    redisClient.set(sessionID, JSON.stringify(context));
     res.status(200).json({ message: "Free model connect success", text });
   } catch (e) {
     next(e);
