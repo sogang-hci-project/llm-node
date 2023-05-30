31d0e2e1e18c766b84529a729e7bc6f831c378c8
Siwon Jeon <87080940+siwonblue@users.noreply.github.com>
2023-05-30 18:08:55 +0900

    [develop/#3-proto] elastic cache redis endpoint test

--- src/lib/redis.ts
+++ src/lib/redis.ts
@@ -1,5 +1,7 @@
 import Redis from "ioredis";
-
+import { isProd } from "~/constants";
+const host = isProd ? process.env.REDIS_END_POINT : "127.0.0.1";
+console.log(process.env.REDIS_END_POINT);
 export const redisClient = new Redis({
-  host: "127.0.0.1",
+  host,
 });
