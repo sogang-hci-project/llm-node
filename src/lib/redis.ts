705afd5659f096f084dc369addef20f5da104256
Siwon Jeon <87080940+siwonblue@users.noreply.github.com>
2023-05-31 17:14:40 +0900

    [develop/#3-proto] redis cluster

--- src/lib/redis.ts
+++ src/lib/redis.ts
@@ -2,6 +2,8 @@ import Redis from "ioredis";
 // import { isProd } from "~/constants";
 // const host = true ? process.env.REDIS_END_POINT : "127.0.0.1";
 console.log(process.env.REDIS_END_POINT);
-export const redisClient = new Redis({
-  host: process.env.REDIS_END_POINT,
-});
+export const redisClient = new Redis.Cluster([
+  {
+    host: process.env.REDIS_END_POINT,
+  },
+]);
